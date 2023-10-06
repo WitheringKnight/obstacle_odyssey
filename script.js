2c38ff8e77fa08f639bead59f158e2393da71d05
Daniel Torelli <daniel.torelli05@gmail.com>
2023-10-06 19:39:04 +0200

    Implementata gestione dei livelli

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 //imposto la gravità del livello
 const gravity = 0.5
+
+let levels = {}
 let gameSpeed = 4
 let gameFrame = 0
 let lives = 3
@@ -30,45 +32,37 @@ let game = document.getElementById('game')
 function init(){
 	
 	gameFrame = 0
-    
-    let levels = {
+    levels = {
         level: [
           {
-            backgrounds: [
-              {
-				  x: 0, y:0, speedMod: 0.2, src: "img/mountains.png"
-				},
-				{
-				   x:0, y: 300, speedMod: 0.5, src: "img/trees.png"
-				 }],
-            platforms: [
-               {x: 450, y: 300, speedMod: 0, src: "img/platform1.png"
-			   },
-               {x: 1250, y: 350, speedMod: 2, src: "img/platform1.png"
-			   },
-               {x: 1650, y: 350, speedMod: 1, src: "img/platform1.png"
-			  },
-               {x: 3350,y: 350, speedMod: 2, src: "img/platform1.png"
-			  },
-               {x: 3650, y: 350, speedMod: 3, src: "img/platform1.png"
-				}],
-            pavements: [
-                {x:0,y:535, src:"img/pavement.png"},
-                {x:700,y:535,src:"img/pavement.png"},
-                {x:2000, y:535,src:"img/pavement.png"},
-                {x:2350, y:535,src:"img/pavement.png"},
-                {x:2750,y: 535,src:"img/pavement.png"},
-                {x:4000,y: 535,src:"img/pavement.png"},
-               ],
-            object: [
-                {x:150,  y:478, src: "img/sasso1.png"},
-                {x:758,  y:460, src: "img/sasso2.png"},
-                {x:4250,  y:460, src: "img/premio.png"}
-            ],
+            backgrounds:[
+				new Layer(0, 0, 0.2, "img/mountains.png"),
+                new Layer(0, 300, 0.5, "img/trees.png")
+				],
+            platforms:[     
+                new Platform(450, 300, 0,"img/platform1.png"),
+                new Platform(1250,350, 2, "img/platform1.png"),
+                new Platform(1650, 350, 1, "img/platform1.png"),
+                new Platform(3350,350, 2, "img/platform1.png"),
+                new Platform(3650, 350, 3, "img/platform1.png")
+                ],
+            pavements:[ 
+                new Pavement(0,535,"img/pavement.png"),
+                new Pavement(700,535,"img/pavement.png"),
+                new Pavement(2000, 535,"img/pavement.png"),
+                new Pavement(2350, 535,"img/pavement.png"),
+                new Pavement(2750,535,"img/pavement.png"),
+                new Pavement(4000,535,"img/pavement.png")
+                ],
+            stones:[
+                new genericObject(150,  478, "img/sasso1.png"),
+                new genericObject(758,  460, "img/sasso2.png"),
+                new genericObject(4250,  460, "img/premio.png")
+                ],
             enemies:[
-                {x:2350,  y: 420, speedMod: 0, src: "img/cactus.png"},
-                {x:2750,  y: 420, speedMod: 1, src: "img/cactus.png"}
-            ]
+                new enemy(2350,  420,0, "img/cactus.png"),
+                new enemy(2750,  420,1, "img/cactus.png")
+                ]
           }] // ricomincia con background per il secondo livello
           } 
     player = new Player()
@@ -103,25 +97,25 @@ function animate(){
     c.clearRect(0,0, canvas.width, canvas.height)
     if((left_pressed || right_pressed) && player.can_jump)
 	passo.play()
-    Background.forEach((Layer) =>{
-		Layer.update()
-		Layer.draw()
-	})
-    pavements.forEach((pavement) =>{
+    levels.level[0].backgrounds.forEach(Layer =>{
+        Layer.update()
+        Layer.draw()
+    })
+    levels.level[0].pavements.forEach(pavement =>{
         pavement.draw()
     })
-	 stone.forEach((stone) =>{
+	levels.level[0].stones.forEach(stone =>{
         stone.draw()
     })
 	player.update()
 	player.draw()
-    platforms.forEach((platform) =>{
-		 platform.update(player)
+    levels.level[0].platforms.forEach(platform =>{
+        platform.update(player)
         platform.draw()
     })
    
-    enemies.forEach((enemy) =>{
-		enemy.update(player)
+    levels.level[0].enemies.forEach(enemy =>{
+        enemy.update(player)
         enemy.draw()
     })
 	printText(c, "white" , 24, "Current lives: " + lives, 1)
@@ -137,21 +131,21 @@ function animate(){
         player.velocity.x = 0
         if(right_pressed){
 			gameFrame--
-            platforms.forEach((platform) =>{
+            levels.level[0].platforms.forEach((platform) =>{
             platform.position.x -= 5
-               })
-            pavements.forEach((pavement) =>{
+            })
+            levels.level[0].pavements.forEach((pavement) =>{
             pavement.position.x -= 5
         })
-        stone.forEach((stone) =>{
+        levels.level[0].stones.forEach((stone) =>{
             stone.position.x -= 5
         })
-        enemies.forEach((enemy) =>{
+        levels.level[0].enemies.forEach((enemy) =>{
             enemy.position.x -= 5
         })
         }
         else if(left_pressed){
-			if(pavements[0].position.x == 0){
+			if(levels.level[0].pavements[0].position.x == 0){
 				inc = 0
 				stop = 0
 			}
@@ -160,22 +154,22 @@ function animate(){
 				stop = 1
 			}
 			gameFrame+= stop
-            platforms.forEach((platform) =>{
+            levels.level[0].platforms.forEach((platform) =>{
             platform.position.x += inc
             })
-            pavements.forEach((pavement) =>{
+            levels.level[0].pavements.forEach((pavement) =>{
             pavement.position.x += inc
             })
-            stone.forEach((stone) =>{
+            levels.level[0].stones.forEach((stone) =>{
                 stone.position.x += inc
         })
-        enemies.forEach((enemy) =>{
+        levels.level[0].enemies.forEach((enemy) =>{
             enemy.position.x += inc
         })
         }
     }
      //collisioni sul pavimento
-    pavements.forEach((pavement) =>{
+    levels.level[0].pavements.forEach((pavement) =>{
     if(player.position.y + player.height <= pavement.position.y 
         && player.position.y + player.height + player.velocity.y >= pavement.position.y 
         //controllo se il giocatore oltrepassa i bordi della piattaforma e lo faccio cadere
@@ -189,10 +183,10 @@ function animate(){
 		}
     })
 	
-        if(player.position.y + player.height >= stone[2].position.y
-            && player.position.y + player.height + player.velocity.y >= stone[2].position.y 
-            && player.position.x + player.width >= stone[2].position.x + 30 
-            && player.position.x + player.width <= stone[2].position.x + stone[2].image.width + 30
+        if(player.position.y + player.height >= levels.level[0].stones[2].position.y
+            && player.position.y + player.height + player.velocity.y >= levels.level[0].stones[2].position.y 
+            && player.position.x + player.width >= levels.level[0].stones[2].position.x + 30 
+            && player.position.x + player.width <= levels.level[0].stones[2].position.x + levels.level[0].stones[2].image.width + 30
             )
             {
                 player.gameover = true
